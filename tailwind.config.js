4520e5a9ce587e0b72ed145b723a2344e9235b0a
mj-creatives <mjcreativesllc@gmail.com>
2022-12-06 22:07:52 -0500

    New Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#8F223B',
+        'secondary': '#F69220',
+        'thirdcolor': '#A64626',
+        'mylightgray': '#AFB5BF',
+        'mydarkgray': '#46464E',
+      },
+    },
+  },
+}
